df7ceab7577bf45695a715f3eddde36d5d15e801
sohanemon <m.sohanemon@gmail.com>
2022-08-16 21:50:38 +0600

    minimal done

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -1,19 +1,17 @@
 var deposited = document.getElementById("deposited").innerText;
 const depositField = document.getElementById("depositField");
 /* adding event listener */
-const depositSubmit = document
-  .getElementById("depositSubmit")
-  .addEventListener("click", () => {
-    if (isNaN(depositField.value)) {
-      alert("নাম্বার দে ");
-      return;
-    }
-    const value = parseFloat(depositField.value);
-    document.getElementById("deposited").innerText =
-      parseFloat(document.getElementById("deposited").innerText) + value;
-    const value2 = parseFloat(depositField.value);
-    document.getElementById("balance").innerText =
-      parseFloat(document.getElementById("balance").innerText) + value;
+document.getElementById("depositSubmit").addEventListener("click", () => {
+  if (isNaN(depositField.value)) {
+    alert("নাম্বার দে ");
+    return;
+  }
+  const value = parseFloat(depositField.value);
+  document.getElementById("deposited").innerText =
+    parseFloat(document.getElementById("deposited").innerText) + value;
+  const value2 = parseFloat(depositField.value);
+  document.getElementById("balance").innerText =
+    parseFloat(document.getElementById("balance").innerText) + value;
 
-    depositField.value = "";
-  });
+  depositField.value = "";
+});
---
+++ scripts/withdraw.js
@@ -0,0 +1,18 @@
+var withdrawal = document.getElementById("withdrawal").innerText;
+const withdrawField = document.getElementById("withdrawField");
+console.log(withdrawField);
+/* adding event listener */
+document.getElementById("withdrawSubmit").addEventListener("click", () => {
+  if (isNaN(withdrawField.value)) {
+    alert("নাম্বার দে ");
+    return;
+  }
+  const value = parseFloat(withdrawField.value);
+  document.getElementById("withdrawal").innerText =
+    parseFloat(document.getElementById("withdrawal").innerText) + value;
+  const value2 = parseFloat(withdrawField.value);
+  document.getElementById("balance").innerText =
+    parseFloat(document.getElementById("balance").innerText) - value;
+
+  withdrawField.value = "";
+});
