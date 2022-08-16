23757aceab9350d91571f48365ab41647dfd9d7f
sohanemon <m.sohanemon@gmail.com>
2022-08-16 21:22:45 +0600

    deposit done

---
+++ scripts/deposit.js
@@ -0,0 +1,19 @@
+var deposited = document.getElementById("deposited").innerText;
+const depositField = document.getElementById("depositField");
+/* adding event listener */
+const depositSubmit = document
+  .getElementById("depositSubmit")
+  .addEventListener("click", () => {
+    if (isNaN(depositField.value)) {
+      alert("নাম্বার দে ");
+      return;
+    }
+    const value = parseFloat(depositField.value);
+    document.getElementById("deposited").innerText =
+      parseFloat(document.getElementById("deposited").innerText) + value;
+    const value2 = parseFloat(depositField.value);
+    document.getElementById("balance").innerText =
+      parseFloat(document.getElementById("balance").innerText) + value;
+
+    depositField.value = "";
+  });
